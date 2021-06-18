ec47783f843cef6f357ab49f6e5c32eebcf189d4
ee904 <KaiYin77>
2021-06-18 21:45:12 +0800

    upload apriltag part

--- hcc_ws/build/catkin_generated/setup_cached.sh
+++ hcc_ws/build/catkin_generated/setup_cached.sh
@@ -9,8 +9,6 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ee904/MOLA/bob_lab/hcc-lab-2021/hcc-final-competition-2021/hcc_ws/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/ee904/MOLA/bob_lab/hcc-lab-2021/hcc-final-competition-2021/hcc_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/ee904/MOLA/bob_lab/hcc-lab-2021/hcc-final-competition-2021/hcc_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/ee904/MOLA/bob_lab/hcc-lab-2021/hcc-final-competition-2021/hcc_ws/build'
-export PYTHONPATH="/home/ee904/MOLA/bob_lab/hcc-lab-2021/hcc-final-competition-2021/hcc_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/ee904/MOLA/bob_lab/hcc-lab-2021/hcc-final-competition-2021/hcc_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/ee904/MOLA/bob_lab/hcc-lab-2021/hcc-final-competition-2021/hcc_ws/src:$ROS_PACKAGE_PATH"
