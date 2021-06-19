72031701a0da7ffa53ba9ed536dc605233339a8f
jerry8137 <jerry.eed07@nctu.edu.tw>
2021-06-19 14:20:34 +0800

    transform done

--- hcc_ws/src/estimation_pos/src/drone_object.py
+++ hcc_ws/src/estimation_pos/src/drone_object.py
@@ -11,11 +11,14 @@ from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import PointStamped
 from nav_msgs.msg import Odometry
 import tf
-from tf.transformations import quaternion_matrix
+from tf.transformations import quaternion_matrix, translation_matrix
+from tf import transformations
 import math
+import os
+import sys
 
 pub = rospy.Publisher('/object_pose', PointStamped, queue_size=10)
-pub1 = rospy.Publisher('/object_pose_on_camera', PointStamped, queue_size=10)
+pub1 = rospy.Publisher('/camera_pose', PointStamped, queue_size=10)
 rospy.init_node('drone_Object', anonymous=True)
 rospy.loginfo("Start D435_Object_Distance")
 cv_bridge = CvBridge()
@@ -35,6 +38,10 @@ cy = msg.P[6]
 transform_time = 0.0
 transform = Odometry()
 
+Umbrella = np.zeros(3)
+Bike = np.zeros(3)
+TeddyBear =  np.zeros(3)
+TB_CloseToChair = np.zeros(3)
 
 def main():
     depth_image_sub = message_filters.Subscriber('/camera/aligned_depth_to_color/image_raw', Image) # ('???', ???)
@@ -58,7 +65,7 @@ def callback(depth_img, bb):
     # print("Get local_time")
     # print(local_time)
     # you could set the time error2, 3, 4, 5 (local_time - transform_time) by yourself    
-    if abs(local_time - transform_time) < 0.5 and transform_time != 0: #??? and transform_time != 0:
+    if abs(local_time - transform_time) < 5 and transform_time != 0: #??? and transform_time != 0:
         print("Time error")
         print(local_time - transform_time)
         
@@ -72,24 +79,25 @@ def callback(depth_img, bb):
         global_transform[0][3] = transform.pose.pose.position.x #???
         global_transform[1][3] = transform.pose.pose.position.y #???
         global_transform[2][3] = transform.pose.pose.position.z #???
-        print("transform")
-        print(global_transform)
+        # print("transform")
+        # print(global_transform)
         try:
             cv_depthimage = cv_bridge.imgmsg_to_cv2(depth_img, "32FC1")
             cv_depthimage2 = np.array(cv_depthimage, dtype=np.float32)
         except CvBridgeError as e:
             print(e)
         
+        # publish camera pos in origin frame
         v1 = np.array([0,0,0,1])
-        object_position = np.dot(global_transform, v1)
-        print("camera_link")
-        print(object_position)
+        org = np.matmul(global_transform, v1)
+        # print("camera_link")
+        # print(object_position)
         point_message = PointStamped()
         point_message.header = depth_img.header
         point_message.header.frame_id = "origin"
-        point_message.point.x = object_position[0]
-        point_message.point.y = object_position[1]
-        point_message.point.z = object_position[2]
+        point_message.point.x = org[0]
+        point_message.point.y = org[1]
+        point_message.point.z = org[2]
         pub1.publish(point_message)
 
         for i in bb.bounding_boxes:
@@ -100,64 +108,78 @@ def callback(depth_img, bb):
                 rospy.loginfo("see umbrella")
                 zc = cv_depthimage2[int(y_mean)][int(x_mean)]
                 v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy))
-                object_position = np.dot(global_transform, v1)
-                point_message = PointStamped()
-                point_message.header = depth_img.header
-                point_message.header.frame_id = "origin"
-                point_message.point.x = object_position[0]
-                point_message.point.y = object_position[1]
-                point_message.point.z = object_position[2]
-                pub.publish(point_message)
+                object_position = np.matmul(global_transform, v1)
+                publish_object_location(object_position,depth_img,org)
+                # point_message = PointStamped()
+                # point_message.header = depth_img.header
+                # point_message.header.frame_id = "origin"
+                # point_message.point.x = object_position[0]
+                # point_message.point.y = object_position[1]
+                # point_message.point.z = object_position[2]
+                # pub.publish(point_message)
             elif i.Class == "bike":
                 rospy.loginfo("see bike")
                 zc = cv_depthimage2[int(y_mean)][int(x_mean)]
                 v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy))
-                object_position = np.dot(global_transform, v1)
-                point_message = PointStamped()
-                point_message.header = depth_img.header
-                point_message.header.frame_id = "origin"
-                point_message.point.x = object_position[0]
-                point_message.point.y = object_position[1]
-                point_message.point.z = object_position[2]
-                pub.publish(point_message)
+                object_position = np.matmul(global_transform, v1)
+                publish_object_location(object_position,depth_img,org)
+                # point_message = PointStamped()
+                # point_message.header = depth_img.header
+                # point_message.header.frame_id = "origin"
+                # point_message.point.x = object_position[0]
+                # point_message.point.y = object_position[1]
+                # point_message.point.z = object_position[2]
+                # pub.publish(point_message)
             elif i.Class == "teddy bear":
                 rospy.loginfo("see teddy bear")
                 zc = cv_depthimage2[int(y_mean)][int(x_mean)]
                 v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy))
-                object_position = np.dot(global_transform, v1)
-                point_message = PointStamped()
-                point_message.header = depth_img.header
-                point_message.header.frame_id = "origin"
-                point_message.point.x = object_position[0]
-                point_message.point.y = object_position[1]
-                point_message.point.z = object_position[2]
-                pub.publish(point_message)                
+                object_position = np.matmul(global_transform, v1)
+                publish_object_location(object_position,depth_img,org)
+                # point_message = PointStamped()
+                # point_message.header = depth_img.header
+                # point_message.header.frame_id = "origin"
+                # point_message.point.x = object_position[0]
+                # point_message.point.y = object_position[1]
+                # point_message.point.z = object_position[2]
+                # pub.publish(point_message)                
             elif i.Class == "chair":
                 rospy.loginfo("see chair")
                 zc = cv_depthimage2[int(y_mean)][int(x_mean)]
                 v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy))
-                object_position = np.dot(global_transform, v1)
-                print(object_position/1000)
-                point_message = PointStamped()
-                point_message.header = depth_img.header
-                point_message.header.frame_id = "origin"
-                point_message.point.x = object_position[0]
-                point_message.point.y = object_position[1]
-                point_message.point.z = object_position[2]
-                pub.publish(point_message)
+                object_position = np.matmul(global_transform, v1)
+                publish_object_location(object_position,depth_img,org)
+                print(v1)
+                # point_message = PointStamped()
+                # point_message.header = depth_img.header
+                # point_message.header.frame_id = "origin"
+                # point_message.point.x = object_position[0]
+                # point_message.point.y = object_position[1]
+                # point_message.point.z = object_position[2]
+                # pub.publish(point_message)
             ############################
             #  Student Implementation  #
             ############################
+            # print ("os.path.realpath(__file__) = ", os.path.realpath('..'))
+            # Write in .txt
+            list = [Umbrella, Bike, TeddyBear, TB_CloseToChair]
+            submission_path = os.path.realpath('..')
+            file = open(submission_path+'/output.txt', 'w')
+            for element in list:
+                file.write(str(element[0]) + " ")
+                file.write(str(element[1]) + " ")
+                file.write(str(element[2]) + "\n")
+            file.close()
 
-# def publish_object_location(object_position):
-#     print(object_position/1000)
-#     point_message = PointStamped()
-#     point_message.header = depth_img.header
-#     point_message.header.frame_id = "origin"
-#     point_message.point.x = object_position[0]/1000
-#     point_message.point.y = object_position[1]/1000
-#     point_message.point.z = object_position[2]/1000
-#     pub.publish(point_message)
+def publish_object_location(object_position, depth_img,org):
+    # print(object_position/1000)
+    point_message = PointStamped()
+    point_message.header = depth_img.header
+    point_message.header.frame_id = "origin"
+    point_message.point.x = object_position[0]/1000 + org[0]
+    point_message.point.y = object_position[1]/1000 + org[1]
+    point_message.point.z = object_position[2]/1000 + org[2]
+    pub.publish(point_message)
 
 def getXYZ(xp, yp, zc, fx, fy, cx, cy):
     #### Definition:
