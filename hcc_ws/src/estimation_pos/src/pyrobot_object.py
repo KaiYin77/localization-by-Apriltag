694f6fa89dc618411bee85911070a575bd0d869c
jerry8137 <jerry.eed07@nctu.edu.tw>
2021-06-19 23:25:43 +0800

    pyrobot works

--- hcc_ws/src/estimation_pos/src/pyrobot_object.py
+++ hcc_ws/src/estimation_pos/src/pyrobot_object.py
@@ -12,8 +12,11 @@ from nav_msgs.msg import Odometry
 import tf
 from tf.transformations import quaternion_matrix
 import math
+import os
+import sys
 
 pub = rospy.Publisher('/object_pose', PointStamped, queue_size=10)
+pub1 = rospy.Publisher('/camera_pose', PointStamped, queue_size=10)
 rospy.init_node('drone_Object', anonymous=True)
 rospy.loginfo("Start D435_Object_Distance")
 cv_bridge = CvBridge()
@@ -33,6 +36,20 @@ cy = msg.P[6]
 transform_time = 0.0
 transform = Odometry()
 
+Dist_Cam_Umb = 100
+Dist_Cam_Umb_cloest = 100
+
+Orange_bottle = np.zeros(3)
+Green_bottle = np.zeros(3)
+Laptop_near_tag1 =  np.zeros(3)
+Backpack = np.zeros(3)
+TeddyBear = np.zeros(3)
+
+Orange_bottle_output = []
+Green_bottle_output = []
+Laptop_output = []
+Backpack_output = []
+TeddyBear_output = []
 
 
 def main():
@@ -40,7 +57,7 @@ def main():
     ## Note that you may need color image to find the color of bottles
     color_image_sub = message_filters.Subscriber('/camera/color/image_raw', Image) #('???', ???)
     bb_sub = message_filters.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes) #('???', ???)
-    ts = message_filters.ApproximateTimeSynchronizer([depth_image_sub, bb_sub], 10, 0.5) #(???, ???, ???)
+    ts = message_filters.ApproximateTimeSynchronizer([depth_image_sub, bb_sub, color_image_sub], 10, 0.5) #(???, ???, ???)
     ts.registerCallback(callback)
     rospy.Subscriber("apriltag_localization", Odometry, transform_cb) #("???", ???, ???)
     rospy.spin()
@@ -50,27 +67,35 @@ def transform_cb(msg):
     global transform
     transform_time = msg.header.stamp.to_sec()
     transform = msg
-    print("Get transform time")
-    print(transform_time)
+    # print("Get transform time")
+    # print(transform_time)
 
-def publish_object_location(location, depth_img):
-    print(location/1000)
+def publish_object_location(object_position, depth_img, org, obj, class_type):
+    global Orange_bottle_output
+    global Green_bottle_output
+    global Laptop_output
+    global Backpack_output
+    global TeddyBear_output
+    # print(object_position/1000)
     point_message = PointStamped()
     point_message.header = depth_img.header
-    point_message.header.frame_id = "camera_color_optical_frame"
-    point_message.point.x = location[0]/1000
-    point_message.point.y = location[1]/1000
-    point_message.point.z = location[2]/1000
+    point_message.header.frame_id = "origin"
+    point_message.point.x = object_position[0]/1000 + org[0]
+    point_message.point.y = object_position[1]/1000 + org[1]
+    point_message.point.z = object_position[2]/1000 + org[2]
+    obj[0] = object_position[0]/1000 + org[0]
+    obj[1] = object_position[1]/1000 + org[1]
+    obj[2] = object_position[2]/1000 + org[2]
     pub.publish(point_message)
 
 
 def callback(depth_img, bb, color_img):
     local_time = depth_img.header.stamp.to_sec()
-    print("Get local_time")
-    print(local_time)
-    print(transform_time)
+    # print("Get local_time")
+    # print(local_time)
+    # print(transform_time)
     # you could set the time error (local_time - transform_time) by yourself    
-    if abs(local_time - transform_time) < 0.1 and transform_time != 0: #??? and transform_time != 0:
+    if abs(local_time - transform_time) < 0.05 and transform_time != 0: #??? and transform_time != 0:
         print("Time error")
 
         # hint: http://docs.ros.org/en/jade/api/tf/html/python/transformations.html
@@ -94,6 +119,19 @@ def callback(depth_img, bb, color_img):
         except CvBridgeError as e:
             print(e)
 
+        # publish camera pos in origin frame
+        v1 = np.array([0,0,0,1])
+        org = np.matmul(global_transform, v1)
+        # print("camera_link")
+        # print(object_position)
+        point_message = PointStamped()
+        point_message.header = depth_img.header
+        point_message.header.frame_id = "origin"
+        point_message.point.x = org[0]
+        point_message.point.y = org[1]
+        point_message.point.z = org[2]
+        pub1.publish(point_message)
+
         for i in bb.bounding_boxes:
             x_mean = (i.xmax + i.xmin) / 2
             y_mean = (i.ymax + i.ymin) / 2
@@ -103,39 +141,49 @@ def callback(depth_img, bb, color_img):
                 color = cv_colorimage2[int(y_mean)][int(x_mean)]
                 if color[0] > color[1]: 
                     rospy.loginfo("see orange bottle")
-                    v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy), 1).reshape([4, 1])
+                    v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy))
                     object_position = np.dot(global_transform, v1)
-                    publish_object_location(object_position, depth_img)
+                    publish_object_location(object_position,depth_img, org, Orange_bottle, i.Class)
                 elif color[0] < color[1]:
                     rospy.loginfo("see green bottle")
-                    v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy), 1).reshape([4, 1])
+                    v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy))
                     object_position = np.dot(global_transform, v1)
-                    publish_object_location(object_position, depth_img)
+                    publish_object_location(object_position,depth_img, org, Green_bottle, i.Class)
             
             elif i.Class == "laptop":
                 rospy.loginfo("see laptop")
                 zc = cv_depthimage2[int(y_mean)][int(x_mean)]
-                v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy), 1).reshape([4, 1])
+                v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy))
                 object_position = np.dot(global_transform, v1)
-                publish_object_location(object_position, depth_img)
+                publish_object_location(object_position,depth_img, org, Laptop_near_tag1, i.Class)
 
             elif i.Class == "backpack":
                 rospy.loginfo("see backpack")
                 zc = cv_depthimage2[int(y_mean)][int(x_mean)]
-                v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy), 1).reshape([4, 1])
+                v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy))
                 object_position = np.dot(global_transform, v1)
-                publish_object_location(object_position, depth_img)
+                publish_object_location(object_position,depth_img, org, Backpack, i.Class)
 
             elif i.Class == "teddy bear":
                 rospy.loginfo("see teddy bear")
                 zc = cv_depthimage2[int(y_mean)][int(x_mean)]
-                v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy), 1).reshape([4, 1])
+                v1 = np.array(getXYZ(x_mean, y_mean, zc, fx, fy, cx, cy))
                 object_position = np.dot(global_transform, v1)
-                publish_object_location(object_position, depth_img)
+                publish_object_location(object_position,depth_img, org, TeddyBear, i.Class)
 
             ############################
             #  Student Implementation  #
             ############################
+            # print ("os.path.realpath(__file__) = ", os.path.realpath('..'))
+            # Write in .txt
+            list = [Orange_bottle, Green_bottle, Laptop_near_tag1, Backpack, TeddyBear]
+            submission_path = os.path.realpath('..')
+            file = open(submission_path+'/output/pyrobot_output.txt', 'w')
+            for element in list:
+                file.write(str(element[0]) + " ")
+                file.write(str(element[1]) + " ")
+                file.write(str(element[2]) + "\n")
+            file.close()
 
 
             
@@ -151,7 +199,7 @@ def getXYZ(xp, yp, zc, fx,fy,cx,cy):
     x = (xp-cx) *  zc * inv_fx
     y = (yp-cy) *  zc * inv_fy
     z = zc
-    return (x,y,z)
+    return (x,y,z,1)
 
 
 if __name__ == '__main__':
